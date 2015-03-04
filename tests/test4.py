de0cdc0617d72bba31da9d01502800ac76bbb8c5
Eric Saari <esaari@shutterstock.com>
2015-03-03 18:50:49 -0700

    finishes up 4th test

--- tests/test4.py
+++ tests/test4.py
@@ -1,12 +1,7 @@
 #Verify the "Share this Ad" feature.
 
-#Verify that the ad counts are correct, even when they are over 100.
-import random
-import re
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -30,14 +25,25 @@ wait.until(
     EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#popup-container-" + str(creativeId) + " [title='Share'] > a"))
 )
 
-#embed()
 
-share_this_ad_input = browser.find_element_by_css_selector("#popup-shell .popup [title='Share'] input")
+share_this_ad_input = browser.find_element_by_css_selector("#popup-container-" + str(creativeId) + " [title='Share'] > input")
 share_this_ad_link = browser.find_element_by_css_selector("#popup-container-" + str(creativeId) + " [title='Share'] > a")
 
 share_this_ad_link.click()
 
+shared_ad_url = share_this_ad_input.get_attribute('value')
+browser.get(shared_ad_url)
 
+wait.until(
+    EC.presence_of_all_elements_located((By.ID, "popup-container-hilight-" + str(creativeId)))
+)
 
-embed()
+new_ad_url = browser.find_element_by_css_selector("#share-this-ad-hilight-" + str(creativeId)).get_attribute('value')
+
+try:
+    assert (shared_ad_url == new_ad_url) == True
+except AssertionError:
+    print "Share this ad feature may not be working"
+else:
+    print "Test Passed: Share this ad link worked and contains original ad"
 
